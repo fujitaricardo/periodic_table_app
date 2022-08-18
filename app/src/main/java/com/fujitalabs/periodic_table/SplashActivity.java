d40bc860e73280bf68f816b7f6f59888a69eb29d
fujitaricardo <ricardofujita1@gmail.com>
2022-08-18 18:23:52 -0300

    Added splash screen

---
+++ app/src/main/java/com/fujitalabs/periodic_table/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.fujitalabs.periodic_table;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(startMainActivity, 3000);
+    }
+
+    private Runnable startMainActivity = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
+}
