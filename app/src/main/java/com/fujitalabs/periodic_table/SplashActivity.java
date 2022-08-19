f950bdca4fac85ced573b92b725bb981cb4e3037
fujitaricardo <ricardofujita1@gmail.com>
2022-08-19 14:08:26 -0300

    Splash screen to fullscreen

--- app/src/main/java/com/fujitalabs/periodic_table/SplashActivity.java
+++ app/src/main/java/com/fujitalabs/periodic_table/SplashActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 public class SplashActivity extends Activity {
 
@@ -11,6 +12,9 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
         new Handler().postDelayed(startMainActivity, 3000);
     }
